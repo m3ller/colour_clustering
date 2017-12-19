ddf99045c76ce637a03ba035522a7110fa446a7f
m3ller <melcanwong@hotmail.com>
2017-12-19 11:43:40 -0800

    Read in image

--- driver.py
+++ driver.py
@@ -1,6 +1,17 @@
 # Main program to run colour clustering
+from PIL import Image
+import numpy as np
 
 def main():
     # Read image
+    #img_path = raw_input().strip()
+    img_path = "./tetris_confetti.jpg"
+    img = np.asarray(Image.open(img_path))
+    n_row, n_col, n_dim = np.shape(img)
+
     # k-clustering.  with Means? Medians? k-means++?
+
     # Display image
+
+if __name__ == "__main__":
+    main()
