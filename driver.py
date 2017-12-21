ed62732735a512be24f885c236082c8adf07c891
m3ller <melcanwong@hotmail.com>
2017-12-21 15:48:07 -0800

    Add distance calculation into k-means. Have not accounted for convergence yet.  Display new and original images

--- driver.py
+++ driver.py
@@ -2,6 +2,7 @@
 from PIL import Image
 import numpy as np
 
+
 """ Find k-representative colours of the image
 """
 def kmeans(img, k):
@@ -13,13 +14,20 @@ def kmeans(img, k):
 
     # For each cluster, compare distance with pixel.  We'll use Euclidean distance
     #TODO: Use while-loop and monitor with convergence
+    for ii in xrange(1):
+        # Calculating mean inner-product
+        mean_prod = np.power(means,2)
+        mean_prod = np.sum(mean_prod, axis=1)
+        mean_prod = np.expand_dims(mean_prod, axis=0)
 
-
-    
-    # Find smallest distance 
+        # Calculating sample/mean inner-product
+        pseudo_dist = np.matmul(img, means.T)   # (n,d) x (d, k)
+        pseudo_dist = pseudo_dist + mean_prod
+        
+        # Find smallest distance 
+        cluster_num = np.argmin(pseudo_dist, axis=1)
     # Return cluster number, cluster mean
-
-    pass
+    return cluster_num, means
 
 def main():
     # Read image
@@ -31,11 +39,17 @@ def main():
 
     # k-clustering.  with Means? Medians? k-means++?
     # recall that medians are more robust to outliers
+    cluster_num, means = kmeans(img, 100)
+    new_img = means[cluster_num]
+    new_img = new_img.reshape((n_row, n_col, n_dim))
     
-
-    # Display image
-    new_img = Image.fromarray(img)
+    # Display new image
+    new_img = Image.fromarray(new_img)
     new_img.show()
+
+    # Display original image
+    img = Image.fromarray(img.reshape((n_row, n_col, n_dim)))
+    img.show()
 
 if __name__ == "__main__":
     main()
