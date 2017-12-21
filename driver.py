515b0cf5eea747892182a42ea3e55342680b2dec
m3ller <melcanwong@hotmail.com>
2017-12-21 14:13:36 -0800

    Adding outline of kmeans(..)

--- driver.py
+++ driver.py
@@ -2,14 +2,36 @@
 from PIL import Image
 import numpy as np
 
+""" Find k-representative colours of the image
+"""
+def kmeans(img, k):
+    # Randomly pick k pixels as initial cluster "means"
+    # Random indices are picked without replacement; to avoid duplicate means
+    n = len(img) 
+    rand_ind = np.random.choice(n, size=k, replace=False) 
+    means = img[rand_ind, :]
+
+    # For each cluster, compare distance with pixel.  We'll use Euclidean distance
+    #TODO: Use while-loop and monitor with convergence
+
+
+    
+    # Find smallest distance 
+    # Return cluster number, cluster mean
+
+    pass
+
 def main():
     # Read image
     #img_path = raw_input().strip()
     img_path = "./tetris_confetti.jpg"
     img = np.asarray(Image.open(img_path))
     n_row, n_col, n_dim = np.shape(img)
+    img = img.reshape((n_row*n_col, n_dim))
 
     # k-clustering.  with Means? Medians? k-means++?
+    # recall that medians are more robust to outliers
+    
 
     # Display image
     new_img = Image.fromarray(img)
