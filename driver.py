07dfa418d7832c318eeeacd221f4c57eeeb474e9
m3ller <melcanwong@hotmail.com>
2017-12-21 17:14:47 -0800

    Update cluster means.  Added -2 to pseudo-distance calculation.  Storing mean colour values as uint8

--- driver.py
+++ driver.py
@@ -14,19 +14,26 @@ def kmeans(img, k):
 
     # For each cluster, compare distance with pixel.  We'll use Euclidean distance
     #TODO: Use while-loop and monitor with convergence
-    for ii in xrange(1):
+    for temp_ind in xrange(1):
         # Calculating mean inner-product
         mean_prod = np.power(means,2)
         mean_prod = np.sum(mean_prod, axis=1)
-        mean_prod = np.expand_dims(mean_prod, axis=0)
+        mean_prod = np.expand_dims(mean_prod, axis=0)   # (1,k)
 
         # Calculating sample/mean inner-product
-        pseudo_dist = np.matmul(img, means.T)   # (n,d) x (d, k)
-        pseudo_dist = pseudo_dist + mean_prod
+        pseudo_dist = np.matmul(img, means.T)   # (n,d) x (d,k)
+        pseudo_dist = -2*pseudo_dist + mean_prod
         
         # Find smallest distance 
         cluster_num = np.argmin(pseudo_dist, axis=1)
-    # Return cluster number, cluster mean
+
+        # Update cluster means
+        means = np.zeros((k, 3), dtype=np.uint8)
+        for ii in xrange(k):
+            cluster_members = img[cluster_num==ii]
+            means[ii,:] = np.floor(np.mean(cluster_members, axis=0)).astype(np.uint8)
+        print means
+
     return cluster_num, means
 
 def main():
@@ -39,11 +46,12 @@ def main():
 
     # k-clustering.  with Means? Medians? k-means++?
     # recall that medians are more robust to outliers
-    cluster_num, means = kmeans(img, 100)
+    cluster_num, means = kmeans(img, 10)
     new_img = means[cluster_num]
-    new_img = new_img.reshape((n_row, n_col, n_dim))
     
     # Display new image
+    new_img = new_img.reshape((n_row, n_col, n_dim))
+    print new_img.shape
     new_img = Image.fromarray(new_img)
     new_img.show()
 
