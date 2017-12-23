a9d44cc57128c4d9a77bd89b1cfe50b418c0ee48
m3ller <melcanwong@hotmail.com>
2017-12-23 13:32:00 -0800

    Clean up on how image casting is done

--- driver.py
+++ driver.py
@@ -10,29 +10,33 @@ def kmeans(img, k):
     # Random indices are picked without replacement; to avoid duplicate means
     n = len(img) 
     rand_ind = np.random.choice(n, size=k, replace=False) 
-    means = img[rand_ind, :]
+    means = img[rand_ind, :].astype(np.float32) 
 
     # For each cluster, compare distance with pixel.  We'll use Euclidean distance
     #TODO: Use while-loop and monitor with convergence
     for temp_ind in xrange(1):
+        # Calculating distance; note that 'means' needs to be converted to float for calculations
         # Calculating mean inner-product
-        mean_prod = np.power(means.astype(np.float32),2)
+        mean_prod = np.power(means, 2)
         mean_prod = np.sum(mean_prod, axis=1)
         mean_prod = np.expand_dims(mean_prod, axis=0)   # (1,k)
 
         # Calculating sample/mean inner-product
-        pseudo_dist = np.matmul(img.astype(np.float32), means.astype(np.float32).T)   # (n,d) x (d,k)
+        #pseudo_dist = np.matmul(img.astype(np.float32), means.T)   # (n,d) x (d,k)
+        pseudo_dist = np.matmul(img, means.T)   # (n,d) x (d,k)
         pseudo_dist = -2*pseudo_dist + mean_prod
         
         # Find smallest distance 
         cluster_num = np.argmin(pseudo_dist, axis=1)
 
         # Update cluster means
-        means = np.zeros((k, 3), dtype=np.uint8)
+        #TODO: worry about potential divide by zero in np.mean
+        means = np.zeros((k, 3), dtype=np.float32)
         for ii in xrange(k):
             cluster_members = img[cluster_num==ii, :]
-            means[ii,:] = np.floor(np.mean(cluster_members, axis=0)).astype(np.uint8)
-
+            means[ii,:] = np.mean(cluster_members, axis=0)
+   
+    means = (np.round(means)).astype(np.uint8)
     return cluster_num, means
 
 def main():
