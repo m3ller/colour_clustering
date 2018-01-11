facb501c1330721fcf0ab07825c9059240703237
m3ller <melcanwong@hotmail.com>
2018-01-10 23:53:37 -0800

    Fix: Kmeans++ bug fix.  Previously, probability was using pseudo_dist, which allowed for negative and postive numbers.  Normally, this is fine, however, since pseudo_dist gets squared, this becomes an issue for NEGATIVE numbers as they are meant to be small in comparison to positive numbers.  By squaring, their probability may become larger than their positive counterparts (not what we want).  As well, 'img' needs to be cast as float32 in the first term of the distance calculation; otherwise we get overflow.

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 # Main program to run colour clustering
 from PIL import Image
 import numpy as np
+import sys
 
 
 """ Find k-representative colours of the image
@@ -15,7 +16,7 @@ def kmeans_driver(img, k, means):
     similarity = 0
     loop_counter = 0    # Counts number of loops needed before reaching convergence
 
-    while similarity < 0.99:
+    while similarity < 0.999:
         # Calculating distance; note that 'means' needs to be converted to float for calculations
         # Calculating mean inner-product
         mean_prod = np.power(means, 2)
@@ -62,8 +63,8 @@ def kmeans_pp(img, k):
 
     # Pick means based on a probability distribution
     dist_mat = np.inf * np.ones((n, k))
-    pseudo_dist = -2*np.matmul(img, means[0,:].T) + np.inner(means[0,:], means[0,:])
-    dist_mat[:,0] = pseudo_dist
+    pseudo_dist = np.sum(np.power(img.astype(np.float32), 2), axis=1) -2*np.matmul(img, means[0,:].T) + np.sum(np.power(means[0,:],2))
+    dist_mat[:,0] = np.abs(pseudo_dist)
 
     for ii in xrange(1, k):
         # Calculate probability
@@ -75,20 +76,32 @@ def kmeans_pp(img, k):
         new_ind = np.random.choice(n, p=prob)
         means[ii,:] = img[new_ind,:]  # new mean
 
+        # Update distance matrix with new mean
+        pseudo_dist = np.sum(np.power(img.astype(np.float32), 2), axis=1) -2*np.matmul(img, means[ii,:].T) + np.sum(np.power(means[ii,:], 2))
+        dist_mat[:,ii] = np.abs(pseudo_dist)
+
     return kmeans_driver(img, k, means)
 
 def main():
+    # Read arguements
+    #TODO: option to pick between kmeans and kmeans++
+    # Usage: python driver.py k-output-colors /path/to/image.jpg
+    k = int(sys.argv[1])
+
+    try:
+        img_path = sys.argv[2]
+    except IndexError:
+        print "No image path given in args; using default image './lego.jpg'"
+        img_path = "./lego.jpg"
+
     # Read image
-    #img_path = raw_input().strip()
-    #img_path = "./tetris_confetti.jpg"
-    img_path = "./lego.jpg"
     img = np.asarray(Image.open(img_path))
     n_row, n_col, n_dim = np.shape(img)
     img = img.reshape((n_row*n_col, n_dim))
 
     # k-clustering.  with Means? Medians? k-means++?
     # recall that medians are more robust to outliers
-    cluster_num, means = kmeans(img, 10)
+    cluster_num, means = kmeans(img, k)
     new_img = means[cluster_num]
 
     # Display original image
